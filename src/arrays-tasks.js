faa95b8d8871ebceeb851bbbf8e33fde436df643
Hanna Kulikowska <hannakulikowska.new@gmail.com>
2024-01-06 12:41:18 +0100

    feat: add solution for sortDigitNamesByNumericOrder task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -529,8 +529,24 @@ function shiftArray(/* arr, n */) {
  *   sortDigitNamesByNumericOrder([ 'nine','eight','nine','eight' ]) => [ 'eight','eight','nine','nine']
  *   sortDigitNamesByNumericOrder([ 'one','one','one','zero' ]) => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const map = new Map();
+  map.set('zero', 0);
+  map.set('one', 1);
+  map.set('two', 2);
+  map.set('three', 3);
+  map.set('four', 4);
+  map.set('five', 5);
+  map.set('six', 6);
+  map.set('seven', 7);
+  map.set('eight', 8);
+  map.set('nine', 9);
+
+  if (arr.length < 1) {
+    return arr;
+  }
+
+  return arr.sort((a, b) => map.get(a) - map.get(b));
 }
 
 /**
